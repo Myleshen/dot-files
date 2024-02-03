0332a8fecaca6a6bce097d9033e08a9fa6eca5b3
Myleshen Praburam <mylesuge@gmail.com>
2024-02-03 13:32:51 +0530

    Add lazygit

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,6 +1,10 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
 
+-- require("java").setup()
+
+-- require("lspconfig").jdtls.setup({})
+
 require("lualine").setup({
   options = {
     theme = "horizon",
